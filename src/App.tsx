952a087b78287c6decb7a1bde8a7e88cdb8eb668
Yaroslav <sewa130391@gmail.com>
2022-04-09 13:39:45 +0300

    HW#34

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Color from './components/Color';
+import InputData from './components/inputData';
+import timeZones from './config/time-zones';
+import Timer from './components/Timer';
 
+type ComponentNames = "input" | "timer" | "color"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [timeZone, setTimeZone] = React.useState("Asia/Jerusalem");
+  const [color, setColor] = React.useState("red"); //TODO setColor
+  const [componentName, setComponentName] = React.useState<ComponentNames>('input');
+  const mapComponents: Map<ComponentNames, React.ReactNode> = new Map ();
+  mapComponents.set("color", <Color color={color}></Color>);
+  mapComponents.set("input", <InputData timeZones={timeZones} injectTimeZone={setTimeZone}></InputData>)
+  mapComponents.set("timer", <Timer timeZone={timeZone}></Timer>)
+    return  <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+      {Array.from(mapComponents.keys()).map(k => <button onClick={() => setComponentName(k)}>{k}</button>)}
+      {mapComponents.get(componentName)}
+      
+      </div>
+  }
 
 export default App;
---
+++ src/components/Timer.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+type Props = {
+    interval?: number,
+    timeZone: string,
+    
+
+}
+const Timer: React.FC<Props> = ({interval, timeZone}) => {
+    const [time, setTime] = React.useState(new Date());
+    function tic():void {
+        console.log("kuku");
+        setTime(new Date());
+    }
+    useEffect(() => {
+        console.log("timer has been mounted");
+        const intervalid = setInterval(tic, interval || 1000);
+        return () => {
+            clearInterval(intervalid);
+            console.log("unmounting timer")};
+    }, [interval])
+    
+    return <div style={{marginLeft: "10vw"}}>
+        <h3>Current Time in {timeZone}</h3>
+        <label>{time.toLocaleTimeString([], {timeZone})}</label>
+    </div>
+}
+export default Timer;
